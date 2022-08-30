c75fabd0754a67acaa49227270b9f66db6a2b94d
tillzmh <tillzmh@users.noreply.github.com>
2022-08-30 19:14:44 -0400

    created data base with info inside (seeds)

--- sql/db/schema.sql
+++ sql/db/schema.sql
@@ -1,16 +1,18 @@
-CREATE DATABASE employee_employees;
+CREATE DATABASE employee_tracker;
+
+USE employee_tracker;
 
 CREATE TABLE department(
     id INT PRIMARY KEY,
     name VARCHAR(30)
-)
+);
 
-CREATE TABLE role(
+CREATE TABLE role (
     id INT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT
-)
+);
 
 CREATE TABLE employee(
     id INT PRIMARY KEY,
@@ -18,4 +20,4 @@ CREATE TABLE employee(
     last_name VARCHAR(30),
     role_id INT,
     manager_id INT NULL
-)
+);
---
+++ sql/db/seeds.sql
@@ -0,0 +1,19 @@
+USE employee_tracker;
+
+INSERT INTO department(id, name) VALUES 
+(1233, 'Networking'),
+(2344, 'Engineering'),
+(3456, 'Desktop'),
+(4789, 'Help Desk');
+
+INSERT INTO role(id, title, salary, department_id) VALUES
+(00001, 'Desktop Tech', 600, 10001),
+(00002, 'System Administrator', 600, 10002),
+(00003, 'Administrator', 600, 10003),
+(00004, 'System analyst', 600, 10004);
+
+INSERT INTO employee(id, first_name, last_name, role_id, manager_id) VALUES
+(00005, 'John', 'Doe', 1991, 10005), 
+(00006, 'Lawrence', 'Tillery', 1992, 10006),
+(00007,'James', 'Doe', 1993, 10007),
+(00008, 'Ermias', 'Doe', 1994, 10009);
